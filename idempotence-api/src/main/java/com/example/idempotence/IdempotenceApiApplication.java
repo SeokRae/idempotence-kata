463046c969f595d0a055c3a04f103441c3c36830
SeokRae <kslbsh@gmail.com>
2023-08-13 15:48:56 +0900

    Initial commit

---
+++ idempotence-api/src/main/java/com/example/idempotence/IdempotenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.idempotence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotenceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdempotenceApiApplication.class, args);
+    }
+
+}
---
+++ idempotence-api/src/test/java/com/example/idempotence/IdempotenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idempotence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotenceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
